ba93634b7890cfe8baa6c5112072a6724d3023e5
qsy123-coder <2175075194@qq.com>
2026-06-09 00:06:58 +0800

    feat: 添加对多种图片格式的模块声明支持

---
+++ global.d.ts
@@ -0,0 +1,26 @@
+// src/global.d.ts
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: string;
+  export default content;
+}
